d4289b02b52dd5dce71572baa4ec0729b9136280
Oded Ben Ozer <oded.benozer@coralogix.com>
2025-10-01 16:36:39 +0200

    more provider specific changes afteer the fork

--- internal/controller/ec2offering.go
+++ internal/controller/ec2offering.go
@@ -21,7 +21,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	"github.com/Oded-B/ec2offering-crossplane-provider/internal/controller/config"
-	"github.com/Oded-B/ec2offering-crossplane-provider/internal/controller/mytype"
 )
 
 // Setup creates all Ec2Offering controllers with the supplied logger and adds them to
@@ -29,7 +28,6 @@ import (
 func Setup(mgr ctrl.Manager, o controller.Options) error {
 	for _, setup := range []func(ctrl.Manager, controller.Options) error{
 		config.Setup,
-		mytype.Setup,
 	} {
 		if err := setup(mgr, o); err != nil {
 			return err
